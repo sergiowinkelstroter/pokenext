b843bf1d1dafe4dfbc15574727ae5e8727177814
Sergio <winksousa0@gmail.com>
2022-12-07 20:04:29 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Card } from "../components/Card";
+import styles from "../styles/Home.module.css";
+
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
+  return (
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image src="/pokeball.png" width={30} height={30} alt="PokeNext" />
+      </div>
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ src/pages/pokemon/[pokemonName].js
@@ -0,0 +1,58 @@
+import styles from "../../styles/Pokemon.module.css";
+
+import Image from "next/image";
+
+export default function Pokemon({ pokemon }) {
+  return (
+    <div className={styles.pokemon_container}>
+      <h1 className={styles.title}>{pokemon.name}</h1>
+      <Image
+        src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+        width="200"
+        height="200"
+        alt={pokemon.name}
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+      <div>
+        <h3>Tipo:</h3>
+        <div className={styles.types_container}>
+          {pokemon.types.map((item, index) => (
+            <span
+              key={index}
+              className={`${styles.type} ${styles["type_" + item.type.name]}`}
+            >
+              {item.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className={styles.data_container}>
+        <div className={styles.data_height}>
+          <h4>Altura:</h4>
+          <p>{pokemon.height * 10} cm</p>
+        </div>
+        <div className={styles.data_weight}>
+          <h4>Peso:</h4>
+          <p>{pokemon.weight / 10} kg</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  let { pokemonName } = context.query;
+
+  if (!pokemonName) {
+    pokemonName = null;
+  }
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+  const data = await res.json();
+
+  return { props: { pokemon: data } };
+};
